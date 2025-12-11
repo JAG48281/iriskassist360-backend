7cc5c866f370bad14b3f499ca1fbf6c94b34d257
Jagdish Challapalli <jaggu.ucpsc07@gmail.com>
2025-12-11 17:39:16 +0530

    Fix Alembic chain: remove reference to deleted revision db53f0e5f521

--- alembic/versions/86c20ab389e1_add_new_premium_rates_endpoints_models.py
+++ alembic/versions/86c20ab389e1_add_new_premium_rates_endpoints_models.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '86c20ab389e1'
-down_revision: Union[str, None] = 'db53f0e5f521'
+down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
